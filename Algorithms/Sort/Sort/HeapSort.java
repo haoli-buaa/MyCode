16d7828ce91c32534dc24ffcf510cbae8485332a
haoli-buaa <haoli_buaa@163.com>
2017-09-22 21:30:14 +0800

    Heap sort

---
+++ Algorithms/Sort/Sort/HeapSort.java
@@ -0,0 +1,62 @@
+package Sort;
+
+import java.util.Scanner;
+
+public class HeapSort {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNextLine()){
+			String[] tmp = sc.nextLine().split(" ");
+			if(tmp.length == 0)
+				continue;
+			int[] a = new int[tmp.length];
+			for(int i = 0; i < a.length; i++){
+				a[i] = Integer.parseInt(tmp[i].trim());
+			}
+			hsort(a);
+			for(int i = 0; i < a.length; i++){
+				System.out.printf("%d", a[i]);
+				if(i != a.length-1)
+					System.out.printf(" ", a[i]);
+				else{
+					System.out.println();
+				}
+			}
+		}
+		sc.close();
+	}
+	public static void hsort(int[] a){
+		//construct max heap
+		for(int k = a.length/2; k >= 0; k--){
+			sink(a, k, a.length);
+		}
+		for(int N = a.length-1; N > 0; N--){
+			swap(a, 0, N);
+			sink(a, 0, N);
+		}
+	}
+	public static void sink(int[] a, int k, int N){
+		while(2*k+1 < N){
+			int j = 2*k + 1;
+			if(2*k + 2 < N && a[2*k + 2] > a[2*k + 1])
+				j++;
+			if(a[k] < a[j]){
+				swap(a, k, j);
+				k = j;
+			}
+			else{
+				break;
+			}
+		}
+	}
+	public static void swap(int[] a, int i, int j){
+		int tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+	}
+}
